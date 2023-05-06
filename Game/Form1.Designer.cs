ee84dded3d0526cf56a9a6cbb3ea325cf3a0a224
uzary <eycopillo@gmail.com>
2023-05-06 12:40:17 -0600

    Trabajo1

--- Game/Form1.Designer.cs
+++ Game/Form1.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            TuboArriva = new PictureBox();
+            Player = new PictureBox();
+            Barrera = new PictureBox();
+            TuvoAbajo = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            timer3 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)TuboArriva).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Barrera).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TuvoAbajo).BeginInit();
+            SuspendLayout();
+            // 
+            // TuboArriva
+            // 
+            TuboArriva.BackgroundImage = Properties.Resources.Tubo_de_arriva;
+            TuboArriva.Location = new Point(220, 12);
+            TuboArriva.Name = "TuboArriva";
+            TuboArriva.Size = new Size(54, 159);
+            TuboArriva.TabIndex = 0;
+            TuboArriva.TabStop = false;
+            // 
+            // Player
+            // 
+            Player.BackgroundImage = Properties.Resources.Mob1;
+            Player.Location = new Point(12, 165);
+            Player.Name = "Player";
+            Player.Size = new Size(133, 103);
+            Player.SizeMode = PictureBoxSizeMode.AutoSize;
+            Player.TabIndex = 1;
+            Player.TabStop = false;
+            Player.Click += Player_Click;
+            // 
+            // Barrera
+            // 
+            Barrera.BackgroundImage = Properties.Resources.vida;
+            Barrera.Location = new Point(-272, 416);
+            Barrera.Name = "Barrera";
+            Barrera.Size = new Size(562, 33);
+            Barrera.TabIndex = 2;
+            Barrera.TabStop = false;
+            // 
+            // TuvoAbajo
+            // 
+            TuvoAbajo.BackgroundImage = Properties.Resources.Tubo_de_abajo;
+            TuvoAbajo.Location = new Point(220, 327);
+            TuvoAbajo.Name = "TuvoAbajo";
+            TuvoAbajo.Size = new Size(54, 83);
+            TuvoAbajo.TabIndex = 3;
+            TuvoAbajo.TabStop = false;
+            TuvoAbajo.Click += TuvoAbajo_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 10;
+            timer2.Tick += timer2_Tick;
+            // 
+            // timer3
+            // 
+            timer3.Enabled = true;
+            timer3.Interval = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.descarga__3;
+            ClientSize = new Size(286, 447);
+            Controls.Add(TuvoAbajo);
+            Controls.Add(Barrera);
+            Controls.Add(Player);
+            Controls.Add(TuboArriva);
+            Name = "Form1";
+            Text = "Juego";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)TuboArriva).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Barrera).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TuvoAbajo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox TuboArriva;
+        private PictureBox Player;
+        private PictureBox Barrera;
+        private PictureBox TuvoAbajo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
     }
 }
--- Game/Form1.cs
+++ Game/Form1.cs
@@ -2,9 +2,70 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        int ContadorMovimientos = 1;
+        bool VolarArriva = false;
+        int Distancia = 0;
+        Random PosicionRandom = new Random();
         public Form1()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+            IniciarJuego();
+        }
+
+        public void IniciarJuego()
+        {
+            Player.Location = new Point(19, 225);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TuvoAbajo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int CantidadMovimientos = 5;
+            if (ContadorMovimientos <= CantidadMovimientos)
+            {
+                Player.Image = Properties.Resources.Mob2;
+                ContadorMovimientos++;
+            }
+            if ((ContadorMovimientos > CantidadMovimientos / 2) && (ContadorMovimientos <= CantidadMovimientos * 2))
+            {
+                Player.Image = Properties.Resources.Mob1;
+                ContadorMovimientos++;
+            }
+            ContadorMovimientos = (ContadorMovimientos > CantidadMovimientos * 2) ? 0 : ContadorMovimientos;
+
+            int ly = Player.Location.Y;
+            int lx = Player.Location.X;
+
+            if (VolarArriva)
+            {
+                ly = ly - 15;
+                VolarArriva = false;
+            }
+            else
+            {
+                ly++;
+            }
+            Player.Location = new Point(Player.Location.X, ly);
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
